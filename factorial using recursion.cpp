38388b75af288cfd829d1b0484ec9082717f70e0
SachinRawat2 <80533220+SachinRawat2@users.noreply.github.com>
2021-03-29 08:05:38 +0530

    Add files via upload

---
+++ factorial using recursion.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int  fact(int n)
+{
+    if(n==0)return 1;
+    return  n*fact(n-1);
+
+}
+
+int main()
+{
+int n,f;
+cin>>n;
+f=fact(n);
+cout<<f;}
